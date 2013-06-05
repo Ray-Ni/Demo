8a54f9644fece1b8eff200569540ba57ff6c0e54
ray <ray@ray-pc.(none)>
2013-06-05 16:48:18 +0800

    well, '}' is forget by me

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ package com.ray.demo
 
 public class App{
 
-public static void main(String[] args){
-    System.out.println("Hello World !");
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
 }
